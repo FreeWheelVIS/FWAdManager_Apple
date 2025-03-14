67a89b16a3b929280875759ac96d8cce2331dfe7
Aaron Friedman <afriedman@freewheel.com>
2025-03-14 11:03:43 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,12 +9,12 @@ let package = Package(
     ],
     products: [
         .library(
-            name: "FWAdManager",
-            targets: ["FWAdManager"]),
+            name: "AdManager",
+            targets: ["AdManager"]),
     ],
     targets: [
         .binaryTarget(
-            name: "FWAdManager",
+            name: "AdManager",
             url: "https://vi.freewheel.tv/static/demos/AdManager.xcframework.zip",
             checksum: "a9c9fccafd3189da9eab1b410fea673801a126bf9bf3b68823de1c4cd3624598"),
     ]
