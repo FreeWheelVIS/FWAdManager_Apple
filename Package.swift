1f0cd2d896f04659426ec62213073b2f11b4720c
FreeWheelVIS <vis@freewheel.tv>
2025-08-07 14:48:08 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AdManager",
-            url: "https://github.com/FreeWheelVIS/FWAdManager_Apple/releases/download/0.0.9/Apple_AdManagerDistribution_0.0.9.zip",
+            url: "https://github.com/FreeWheelVIS/FWAdManager_Apple/releases/download/0.10.0/Apple_AdManagerDistribution_0.10.0.zip",
             checksum: "925954f142b8c11dfe2391130517c56402628ab27c3c0ca24c5adb1b4d21ee18"),
     ]
 )
