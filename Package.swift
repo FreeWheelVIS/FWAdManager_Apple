2ad85dd0ae37b6a71734687a0c8201104330f160
FreeWheelVIS <vis@freewheel.com>
2025-08-15 18:39:48 +0000

    Release 0.0.8: Update CHANGELOG.md and Package.swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AdManager",
-            url: "https://github.com/FreeWheelVIS/FWAdManager_Apple/releases/download/0.10.0/Apple_AdManagerDistribution_0.10.0.zip",
-            checksum: "ce6f51f2cb9daeaff43019b7b54c02bb920cab8c0ca613b92249923e392d5059"),
+            url: "https://github.com/FreeWheelVIS/FWAdManager_Apple/releases/download/0.0.8/Apple_AdManagerDistribution_0.0.8.zip",
+            checksum: "a5f9274ea6cef54d8b84ff94ee9b2f5c6aeac2d0866a13f18f404ae175a156ed"),
     ]
 )
